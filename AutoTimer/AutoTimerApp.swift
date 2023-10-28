04575563d2a50c35c69396dcafc79b21a58c8c91
Jonas Helmer <jonas@photangralenphie.com>
2023-10-28 19:30:01 +0200

    Initial Commit

---
+++ AutoTimer/AutoTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoTimerApp.swift
+//  AutoTimer
+//
+//  Created by Jonas Helmer on 28.10.23.
+//
+
+import SwiftUI
+
+@main
+struct AutoTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
